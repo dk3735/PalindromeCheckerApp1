8a421341bb52befcad9fcc2663820cc33ed0ee42
dk3735 <dk3735@srmist.edu.in>
2026-02-23 16:30:12 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,11 @@
-public class UC1PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker App!");
         System.out.println("Version: 1.0.0");
         System.out.println("This app checks whether a string is a palindrome.");
     }
+}
+
+void main() {
 }
