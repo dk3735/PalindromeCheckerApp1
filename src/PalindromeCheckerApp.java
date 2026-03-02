aded517d49cb3c2199e9464984e132f6f8386119
dk3735 <dk3735@srmist.edu.in>
2026-03-02 15:13:08 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,50 @@
+
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker App!");
         System.out.println("Version: 1.0.0");
         System.out.println("This app checks whether a string is a palindrome.");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
 
-void main() {
-}
+        System.out.println("=== Case-Insensitive & Space-Ignored Palindrome Checker ===");
+        System.out.print("Enter a string: ");
+
+        String input = scanner.nextLine();
+
+        input = input.toLowerCase();
+
+        input = input.replaceAll("\\s+", "");
+
+        boolean result = isPalindrome(input);
+
+        if (result) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+
+
+
+    public static boolean isPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    }
+
+
