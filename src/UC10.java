8a94752d8809e280e81daf222d7b5508377256b5
dk3735 <dk3735@srmist.edu.in>
2026-03-02 15:25:42 +0530

    UC10 - Case-Insensitive & Space-Ignored Palindrome

---
+++ src/UC10.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class UC10
+{
+
+    public static boolean isPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Case-Insensitive & Space-Ignored Palindrome Checker ===");
+        System.out.print("Enter a string: ");
+
+        String input = scanner.nextLine();
+
+        input = input.toLowerCase();
+
+        input = input.replaceAll("
+        boolean result = isPalindrome(input);
+
+        if (result) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
